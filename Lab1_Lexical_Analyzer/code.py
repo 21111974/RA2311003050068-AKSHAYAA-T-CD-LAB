8e792e0543ab280444fe9f68671cc8c11ccfa8e3
Akshayaa T <akshayaathamaraisivakumar@gmail.com>
2026-05-05 20:25:39 +0530

    Update code.py

--- Lab1_Lexical_Analyzer/code.py
+++ Lab1_Lexical_Analyzer/code.py
@@ -1,13 +1,22 @@
-keywords = {"int", "float", "if", "else", "while"}
+# Lexical Analyzer Program
 
-code = input("Enter code: ").split()
+keywords = {"int", "float", "if", "else", "while", "return"}
+
+operators = {"+", "-", "*", "/", "=", "==", "<", ">", "<=", ">="}
+delimiters = {"(", ")", "{", "}", ";", ","}
+
+code = input("Enter source code: ").split()
 
 for word in code:
     if word in keywords:
         print(word, "-> Keyword")
-    elif word.isidentifier():
-        print(word, "-> Identifier")
+    elif word in operators:
+        print(word, "-> Operator")
+    elif word in delimiters:
+        print(word, "-> Delimiter")
     elif word.isdigit():
         print(word, "-> Constant")
+    elif word.isidentifier():
+        print(word, "-> Identifier")
     else:
-        print(word, "-> Operator")
+        print(word, "-> Unknown")
